0a9e13edf66d66a793af4850de91629ca08d000d
sgbienaime <sgbienaime@github.com>
2019-07-22 20:26:34 -0400

    Initial commit

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip musicClip;
+
+    public AudioSource musicSource;
+
+
+    void Start()
+    {
+        musicSource.clip = musicClip;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+          musicSource.Play();
+         
+
+
+
+        
+        
+    }
+}
